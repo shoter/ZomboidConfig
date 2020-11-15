4531c71791a25d143f22c8fa513e70d788cda03a
shoter <lubiebardzozelki@gmail.com>
2020-11-15 12:18:59 +0100

    first commit

---
+++ servertest_spawnregions.lua
@@ -0,0 +1,11 @@
+function SpawnRegions()
+	return {
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "BedfordFalls", file = "media/maps/BedfordFalls/spawnpoints.lua" },
+		{ name = "li_township", file = "media/maps/li_township/spawnpoints.lua" },
+		{ name = "South Muldraugh", file = "media/maps/South Muldraugh/spawnpoints.lua" },
+	}
+end
